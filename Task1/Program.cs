d1a0bf5f318e0745150eeed8ed89344632034ba5
seif <inportant.learn11@gmail.com>
2026-02-09 19:57:57 +0300

    amendment end

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,7 @@ applying the discount based on the following conditions:
 • If the bill amount is between 300 and 499, apply a 10% discount 
 • If the bill amount is less than 300, no discount is applied 
 Finally, print the final price after the discount.*/
-
+double finalamount = 0;
 Console.WriteLine("pleas Writ bill amount");
 double  amount = Convert.ToInt32(Console.ReadLine());
  if (amount <0)
@@ -21,20 +21,23 @@ double  amount = Convert.ToInt32(Console.ReadLine());
 }
 else if (amount >= 500)
 {
-    Console.WriteLine(" 20% discount ");
-
-    amount *= 0.20;
+    double s = amount * 0.20;
+    finalamount = amount - s;
+    Console.WriteLine($" 20% discount finalamount= {finalamount}");
 }
 else if (amount >= 300 && amount < 499)
 {
-    Console.WriteLine(" 10% discount ");
+  
 
-    amount *= 0.10;
+    double s = amount *0.10 ;
+    finalamount = amount - s ;
+    Console.WriteLine($" 10% discount finalamount= {finalamount}");
 }
 
 
 else {
     Console.WriteLine(" no discount is applied ");
+    Console.WriteLine(amount);
 }
-Console.WriteLine(amount);
+
 
